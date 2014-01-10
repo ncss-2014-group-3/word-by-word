ca3fcbdf1d1e30c7b16087af36854e47889be6b1
Nathan Hardy <nhardy@live.com.au>
2014-01-10 12:34:48 +1100

    added build trees and get methods to story.py

--- database/story.py
+++ database/story.py
@@ -1,4 +1,22 @@
+from collections import deque
 class Story:
-    def __init__(self, story_id, title):
+    def __init__(self, story_id, title, first_word):
         self._story_id = story_id
         self._title = title
+        self._first_word = first_word
+        self._story = []
+
+        # Recursive tree building
+        todo = deque(self._first_word])
+        while todo:
+            current = todo.popleft()
+    def _build_tree(self,word):
+        for child in word.children():
+            # _build_tree
+    def story_id(self):
+        return self._story_id
+    def title(self):
+        return self._title
+    
+    def story(self):
+        return ' '.join(self._story)
