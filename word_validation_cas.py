6e8c82fc8f990644f389c9d3daf138a48bdf6fce
Romany Montgomery <romisromany@gmail.com>
2014-01-10 17:45:40 +1100

    Added punctuation validation

--- word_validation_cas.py
+++ word_validation_cas.py
@@ -2,5 +2,7 @@ if " " in word:
     print("This word is invalid.")
 elif #numbers:
     print("This word is invalid.")
+elif: #punctuation:
+    print("This word is invalid.")
 else:
     print(word[0].upper() + word[1:]) 
