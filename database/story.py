007cf8035dc84a8ff6d64e6c5edc1f70331b80f0
Nathan Hardy <nhardy@live.com.au>
2014-01-10 16:55:03 +1100

    Updated story.py for database

--- database/story.py
+++ database/story.py
@@ -1,19 +1,19 @@
-from collections import deque
 import sqlite3
-db = sqlite3.connect('database.db')
 
 class Story:
-    def __init__(self, story_id, title, first_word):
-        self._story_id = story_id
+    def __init__(self, title, first_word, story_id=None):
         self._title = title
         self._first_word = first_word
-    def total_votes(self):
+        self._story_id = story_id
+        db = sqlite3.connect('database.db')
         cursor = db.cursor()
-        cursor.execute('''SELECT COUNT(*) FROM votes WHERE storyID=?''', (self._story_id))
+    def total_votes(self):
+        cursor.execute('''SELECT COUNT(*) FROM votes WHERE storyID=?''', (self._story_id,))
     def story_id(self):
         return self._story_id
     def title(self):
         return self._title
-    
-    def story(self):
-        return ' '.join(self._story)
+    def save(self):
+        if not self._story_id:
+            cursor.execute('''INSERT INTO stories (name) VALUES (?)''', (self._title,))
+            self._story_id = cursor.lastrowid
