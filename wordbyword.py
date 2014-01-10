c8d29dd6e435c68a2c56f21afd8cb50e0c082b68
Chris Hansen <chris@gibsonsec.org>
2014-01-10 17:28:52 +1100

    Semantic HTML.

--- wordbyword.py
+++ wordbyword.py
@@ -11,21 +11,21 @@ server = Server()
 
 def render_word(word):
 	children = word.children()
-	if len(children) == 0:
-		html = """\
+	if not children:
+		html = """
 		<div id="child-{cid}">
 			{word}
 		</div>
-		""".format(cid=word.id(), word=word.value())
+		""".format(cid=word.id(), word=str(word))
 		return html
 	else:
-		children_html = [render_word(child) for child in children]
-		html = """\
+		children_html = (render_word(child) for child in children)
+		html = """
 		<div id="child-{cid}">
 			{word}
 			{children}
 		</div>
-		""".format(cid=word.id(), word=word.value(), children="\n".join(children_html))
+		""".format(cid=word.id(), word=str(word), children="\n".join(children_html))
 		return html
 
 def story(response, sid):
@@ -33,21 +33,16 @@ def story(response, sid):
 	# title, words = db.story_from_id(sid)
 	# current = "Once upon a time there was a swag-master called Alex"
 	html = """
-	<!DOCTYPE html>
-	<html>
-	<head>
-		<title>{title} - Word by Word</title>
-	</head>
-	<body>
-		<h1>{title}</h1>
-		<h3>{current}</h3>
-		<p id="tree">
-			{tree}
-		</p>
-	</body>
-	</html>
+	<header id="something">
+		<h1 id="main-title">{title}</h1>
+		<h3 id="current-sentence">{current}</h3>
+	</header>
+	<content id="tree">
+		{tree}
+	</content>
 	""".format(title=story.title(), current=story.current(), tree=render_word(story.first_word()))
 	response.write(html)
+	return html
 
 if __name__ == "__main__":
 	server.run()
