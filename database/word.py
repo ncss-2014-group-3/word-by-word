cfbed489faf40effbfd5c24d1c30f383641d6804
jyelewis <jye@jyelewis.com>
2014-01-10 16:30:32 +1100

    Updated db word object

--- database/word.py
+++ database/word.py
@@ -3,5 +3,21 @@ import sqlite3
 db = sqlite3.connect("database.db")
 
 
-class word:
-    
+class Word:
+    @classmethod
+    def word_by_id(cla, word_id):
+        c = db.cursor()
+        c.execute("SELECT wordID, storyID, word FROM words WHERE wordID = ?", (word_id,))
+        result = c.fetchone()
+        if result:
+            return cla(result["wordID"],result["storyID"], result["word"])
+            
+        c.commit()
+        return result
+        
+    def __init__(self, id, story_id, word):
+        self._id = id
+        self._story_id = story_id
+        self._word = word
+        
+   
