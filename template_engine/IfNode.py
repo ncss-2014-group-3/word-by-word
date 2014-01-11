d94eb89fc1f13dd66959533593de7b44985e1b40
AdGold <agold@hotmail.com>
2014-01-11 12:47:09 +1100

    started else tag

--- template_engine/IfNode.py
+++ template_engine/IfNode.py
@@ -1,15 +1,16 @@
 class IfNode:
-    def __init__(self, predicate, group):
+    def __init__(self, predicate, group_true, group_false = None):
         self.predicate = predicate
-        self.group = group
+        self.group_true = group_true
+        self.group_false = group_false
 
     def render(self, context):
         condition = eval(self.predicate, {}, context)
         
         if condition:
-            return self.group.render(context)
+            return self.group_true.render(context)
         else:
-            return ''
+            return self.group_false.render(context) if self.group_false is not None else ''
 
     def __repr__(self):
         return 'IfNode({!r})'.format(self.children)
