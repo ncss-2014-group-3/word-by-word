de02ac40d6686dec7118d7269dc834a2b728a50f
Green Sheep <ncss-2014-group-3>
2014-01-11 23:07:32 +1100

    Removed non-functional files from base directory

--- word_validation_cas.py
+++
@@ -1,8 +0,0 @@
-if " " in word:
-    print("This word is invalid.")
-elif #numbers:
-    print("This word is invalid.")
-elif: #punctuation:
-    print("This word is invalid.")
-else:
-    print(word[0].upper() + word[1:]) 
