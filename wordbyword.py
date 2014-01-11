2ae6af21576e3d247b15fc9ca318c4461cc58902
Nicholas Buckeridge <bucknich@gmail.com>
2014-01-11 12:15:29 +1100

    updates
    more updates

--- wordbyword.py
+++ wordbyword.py
@@ -11,35 +11,47 @@ def index(response):
 #	description:
 #		When the page is called for listing the stories avaliable.
 def stories(response):
+	#list will contain:
+	# title, burb and word count
 	#stories = db.story_list()
 
-	stories = ['The big bug','Harry Potter','The Green Sheep','The long snake','the small ant','the broken wheel']
+	#require db.story_list_data()
+	# arguments: no args
+	# returns: title, short burb and word count
+
+
+
+	stories = [
+	("The big bug","big bug wanted a big hug",1002),
+	("Harry Potter","the boy who lived",5000),
+	("The Green Sheep","thomas's bedtime story",1450),
+	("The long snake","pun on python coding",6354),
+	("the small ant","got squashed",3),
+	("the broken wheel","went round and round and fell down",789),
+	]
 	# story_list_data should return: 
 	#	titles
 
-	#db.story_list_data()
-	# arguments:
-	# returns: 
-	response.write("""
-		<html>
-		<head>
-		</head>
-		<body>
+	
+	with open('head.html', 'r') as f:
+		response.write(f.read())
 
-		""")
-	for i in stories:
-		response.write("<h1>")
-		response.write(i)
-		response.write("</h1>")
-	response.write("""
-		</body>
-		</html>
+	for title, blurb, count in stories:
+		response.write("""
+			<div class="story_list">
+			<div class="title">{title}</div>
+			<div class="blurb">{blurb}</div>
+			<div class="count">{count}</div>
+			</div>""".format(title=title, blurb=blurb, count=count))
 
-		""")
+	with open('foot.html', 'r') as f:
+		response.write(f.read())
 
 
+def style(response):
+	with open('style.css', 'r') as f:
+		response.write(f.read())
 server= Server()
-server.register("/hello/([a-z]+)", hello)
 server.register("/", stories)
-server.register("/greet", greet)
+server.register("/style.css", style)
 server.run()
