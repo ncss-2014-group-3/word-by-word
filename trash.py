b8e135db6801cebc2131f2942bebb884c091a8ed
Chris Hansen <chris@gibsonsec.org>
2014-01-11 12:43:32 +1100

    Added some junk. Changed some junk. Lots of junk.

--- trash.py
+++ trash.py
@@ -1,30 +1,32 @@
 # Things we don't really need - just in case.
-def index(response):
-	response.write("Hello, World!<br>")
-	for i in range(50):
-		response.write("{0} ".format(str(i+1)))
+# def index(response):
+# 	response.write("Hello, World!<br>")
+# 	for i in range(50):
+# 		response.write("{0} ".format(str(i+1)))
 
-def hello(response, name):
-	response.write("Hello " + name.title())
+# def hello(response, name):
+# 	response.write("Hello " + name.title())
 
-def greet(response):
-	first = response.get_field("fname", "John")
-	last = response.get_field("lname", "Smith")
-	response.write("Sup, {0} {1}.".format(first, last))
+# def greet(response):
+# 	first = response.get_field("fname", "John")
+# 	last = response.get_field("lname", "Smith")
+# 	response.write("Sup, {0} {1}.".format(first, last))
 
-server.register("/", index)
-server.register("/hello/([a-z]+)", hello)
-server.register("/greet", greet)
+# server.register("/", index)
+# server.register("/hello/([a-z]+)", hello)
+# server.register("/greet", greet)
 
 
 ##################
 import random
 class Word:
-	def __init__(self):
-		self._children = []
+	def __init__(self, v, c=None):
+		self._children = c if c is not None else []
 		self.children = lambda: self._children
-		self.value = lambda: random.choice(["foo", "bar", "baz", "qux", "test"])
+		self.value = lambda: v
 		self.id = lambda: random.randint(0, 50)
+	def __str__(self):
+		return self.value()
 
 class Story:
 	def __init__(self, w):
@@ -32,17 +34,55 @@ class Story:
 		self.first_word = lambda: w
 		self.current = lambda: "Once upon a time"
 
-w2 = Word()
-w2._children = [
-	Word(),
-	Word(),
-	Word()
-]
+foo = Word("once", [
+		Word("there", [
+			Word("was", [
+				Word("a", [
+					Word("frog"),
+					Word("douchebag")
+				])
+			])
+		]),
+		Word("upon", [
+			Word("penis", [
+				Word("land", [
+					Word("needed")
+				])
+			]),
+			Word("a", [
+				Word("time", [
+					Word("Tim", [
+						Word("thought")
+					]),
+					Word("there", [
+						Word("be", [
+							Word("dragons")
+						]),
+						Word("was", [
+							Word("a", [
+								Word("cat"),
+								Word("chicken")
+							])
+						])
+					]),
+				])
+			])
+		])
+	])
 
-w = Word()
-w._children = [
-	Word(),
-	w2
-]
+s = Story(foo)
 
-s = Story(w)
+# w2 = Word()
+# w2._children = [
+# 	Word(),
+# 	Word(),
+# 	Word()
+# ]
+
+# w = Word()
+# w._children = [
+# 	Word(),
+# 	w2
+# ]
+
+# s = Story(w)
--- wordbyword.py
+++ wordbyword.py
@@ -4,36 +4,42 @@ import textwrap
 
 from tornado.ncss import Server
 
-import template
+import template_engine
 import db
 
 # debugging
-from trash import w, s
+# from trash import s
 
-server = Server()
-
-def render_word(word):
+def render_word(word, title=False):
 	children = word.children()
-	if not children:
-		html = """
-		<div id="child-{cid}" class="child">
-			<span class="word">{word}</span>
-		</div>
-		""".format(cid=word.id(), word=str(word))
-		return html
+	if children:
+		children_html = '<td class="children">{}</td>'.format("\n".join(render_word(child) for child in children))
 	else:
-		children_html = (render_word(child) for child in children)
-		html = """
-		<div id="child-{cid}" class="child">
-			<span class="word">{word}</span>
-			<div class="children">{children}</div>
-		</div>
-		""".format(cid=word.id(), word=str(word), children="\n".join(children_html))
-		return html
+		# TODO: why won't this branch happen? :(
+		children_html = ""
+	tht = """
+	<table id="child-{cid}" class="child">
+		<tr>
+			<td class="word">{word}</td>
+			<td class="children">{children}</td>
+		</tr>
+	</table>
+	""".format(
+		cid=int(word.id()),
+		word=(html.escape(str(word).title())
+			  if title
+			  else html.escape(str(word))),
+		children=children_html)
+	return tht
 
-def story(response, sid):
+def story(response, sid=1):
 	# TODO: which do we want?
 	# story = db.Story.from_id(sid)
+
+	# TODO: REMOVE THIS COMPLETE AND UTTER TRASH
+	with open("HTML/view_story.html") as f:
+		x = f.read()
+
 	story = s  # debugging
 	html = """
 	<header id="something">
@@ -43,9 +49,11 @@ def story(response, sid):
 	<content id="story-tree">
 		{tree}
 	</content>
-	""".format(title=story.title(), current=story.current(), tree=render_word(story.first_word()))
+	""".format(title=story.title(), current=story.current(), tree=render_word(story.first_word(), title=True))
 	# TODO: response.write(template.render("view_story.html", story=html))
-	response.write(html)
+	response.write(x.format(x=html))
 
 if __name__ == "__main__":
+	server = Server()
+	server.register("/", story)
 	server.run()
