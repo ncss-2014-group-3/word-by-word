32a25fd6ae52414f121825ddc40da25bc453139c
AdGold <agold@hotmail.com>
2014-01-12 11:42:39 +1100

    added else statement

--- template_engine/IfNode.py
+++ template_engine/IfNode.py
@@ -6,11 +6,12 @@ class IfNode:
 
     def render(self, context):
         condition = eval(self.predicate, {}, context)
-        
         if condition:
             return self.group_true.render(context)
+        elif self.group_false is not None:
+            return self.group_false.render(context)
         else:
-            return self.group_false.render(context) if self.group_false is not None else ''
+            return ''
 
     def __repr__(self):
         return 'IfNode({!r})'.format(self.children)
