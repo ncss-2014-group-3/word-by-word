a25c8200037b3641eae688d5b34a5586f2833a04
Isaac Wallis <itw97@yahoo.com.au>
2014-01-12 14:20:41 +1100

    Create the database iff it doesn't exist.

--- database/__init__.py
+++ database/__init__.py
@@ -1,8 +1,13 @@
 import sqlite3
+import os
+
+# Check if we have an existing DB.
+should_create_db = not os.path.exists('database.db')
 
 connection = sqlite3.connect('database.db')
 
-def create():
+# If we didn't have an existing DB, create an empty one.
+if should_create_db:
   create_sql = open('database/createDatabase.sql').read()
   cursor = connection.cursor()
   for statement in create_sql.split(';'):
