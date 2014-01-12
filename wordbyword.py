4e0c0ea5149250a44eaf1bf1e6cbbfcd3f7631bf
Chris Hansen <chris@gibsonsec.org>
2014-01-13 04:51:21 +1100

    Prune branches properly!

--- wordbyword.py
+++ wordbyword.py
@@ -96,6 +96,7 @@ def view_story(response, sid):
     s = story.Story.from_id(sid)
     if not s:
         raise tornado.web.HTTPError(404)
+
     p = Parser.from_file("templates/viewstory.html")
     # html = """
     # """.format(
@@ -137,6 +138,7 @@ def add_word(response, sid, wid):
         
     if not errors: #if there are no errors
         w.add_child(new_word, author)
+        s.prune()
         response.redirect("/story/" + str(s.story_id))
         return
 
