719cb3a06facf8bebde77cdedfaa310f74b3510b
Nathan Hardy <nhardy@live.com.au>
2014-01-12 11:42:49 +1100

    Removed database_tutorial.py

--- database_tutorial.py
+++
@@ -1,7 +0,0 @@
-##from database.story import Story
-##for story_obj in Story.story_list():
-##    print('Title:',story_obj.title)
-##    print('Votes:',story_obj.total_votes)
-##    print('First word:',story_obj.first_word.value)
-##    print('Word count:',story_obj.word_count)
-##
